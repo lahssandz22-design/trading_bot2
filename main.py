43684398ebe5123e20add8a32d2cae05ff06aa2f
lahssandz22-design <lahssandz22@gmail.com>
2026-09-25 11:01:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ server_thread.daemon = True
 server_thread.start()
 
 # ==========================================
-# 1. إعدادات البوت والمنصة والأمان
+# 1. إعدادات البوت والمنصة والأمان (الرموز الصحيحة)
 # ==========================================
-TELEGRAM_BOT_TOKEN = "887254346:AAFFbiuKXkZsity3lhfE7e7N2jg2aMfSDRI"
+TELEGRAM_BOT_TOKEN = "869642227:AAGNB88pBF_kJzEVBLzFQrBGv7yRG5f3Js4"
 AUTHORIZED_CHAT_ID = "7895743860"
 
 SYMBOLS = [
