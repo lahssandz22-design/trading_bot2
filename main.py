2f06818f6235a5c1ee7f610339361d6dbd39bdaf
lahssandz22-design <lahssandz22@gmail.com>
2026-09-25 10:49:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ server_thread.start()
 # ==========================================
 # 1. إعدادات البوت والمنصة والأمان
 # ==========================================
-TELEGRAM_BOT_TOKEN = "869642227:AAGNB88pBF_kJzEVBLzFQrBGv7yRG5f3Js4"
+TELEGRAM_BOT_TOKEN = "887254346:AAFFbiuKXkZsity3lhfE7e7N2jg2aMfSDRI"
 AUTHORIZED_CHAT_ID = "7895743860"
 
 SYMBOLS = [
